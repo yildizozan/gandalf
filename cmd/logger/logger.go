aebd4b658e363a58a865e3981f40f61a1798db20
Ozan Yıldız <developer@yildizozan.com>
2021-01-27 01:05:18 +0300

    Logger end-point is fixed

--- cmd/logger/logger.go
+++ cmd/logger/logger.go
@@ -27,6 +27,7 @@ func Log(format string, v ...interface{}) {
 func CreateLog(ip string, method string, path string, scheme string, code string) string {
 	return fmt.Sprintf("%s %s %s %s %s %s", ip, time.Now(), method, path, scheme, code)
 }
+
 func Logger(service string, malware bool, malwareType string, log string) {
 	entry := StreamEntry{
 		Stream: Stream{
@@ -49,7 +50,7 @@ func Logger(service string, malware bool, malwareType string, log string) {
 	req.Header.SetContentType("application/json")
 	req.SetBody([]byte(fmt.Sprintf("%s\n", prettyJSON)))
 	req.Header.SetMethodBytes([]byte("POST"))
-	req.SetRequestURIBytes([]byte(viper.GetString("app.logger")))
+	req.SetRequestURIBytes([]byte(viper.GetString("app.logger.loki")))
 	res := fasthttp.AcquireResponse()
 	if err := fasthttp.Do(req, res); err != nil {
 		Log("Log not send!\n")
