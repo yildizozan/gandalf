a8d2e1a2c393425b7c35bcaf23b5edae392f8a23
Ozan Yıldız <developer@yildizozan.com>
2021-01-27 10:00:50 +0300

    New metrics was added

--- cmd/metrics/prometheus.go
+++ cmd/metrics/prometheus.go
@@ -28,10 +28,52 @@ var HttpRequestsTotalVulnerable = prometheus.NewCounterVec(
 		Help: "Number of total vulnerable http requests.",
 	}, []string{"app", "scheme", "code"})
 
+var HttpRequestsSQLInjectionVulnerable = prometheus.NewCounterVec(
+	prometheus.CounterOpts{
+		Name: "gandalf_http_requests_sqlinjection_vulnerable",
+		Help: "Number of total vulnerable http requests.",
+	}, []string{"app", "scheme", "code"})
+
+var HttpRequestsXSSVulnerable = prometheus.NewCounterVec(
+	prometheus.CounterOpts{
+		Name: "gandalf_http_requests_xss_vulnerable",
+		Help: "Number of total vulnerable http requests.",
+	}, []string{"app", "scheme", "code"})
+
+var HttpRequestsIpWhitelist = prometheus.NewCounterVec(
+	prometheus.CounterOpts{
+		Name: "gandalf_http_requests_ip_whitelist",
+		Help: "Number of total vulnerable http requests.",
+	}, []string{"app", "scheme", "code"})
+
+var HttpRequestsIpBlacklist = prometheus.NewCounterVec(
+	prometheus.CounterOpts{
+		Name: "gandalf_http_requests_ip_blacklist",
+		Help: "Number of total vulnerable http requests.",
+	}, []string{"app", "scheme", "code"})
+
+var HttpRequestsHeaderFilter = prometheus.NewCounterVec(
+	prometheus.CounterOpts{
+		Name: "gandalf_http_requests_header_filter",
+		Help: "Number of total vulnerable http requests.",
+	}, []string{"app", "scheme", "code"})
+
+var HttpRequestsPathFiler = prometheus.NewCounterVec(
+	prometheus.CounterOpts{
+		Name: "gandalf_http_requests_path_filter",
+		Help: "Number of total vulnerable http requests.",
+	}, []string{"app", "scheme", "code"})
+
 func Init() {
 	r.MustRegister(HttpRequestsTotal)
 	r.MustRegister(HttpRequestsTotalVulnerable)
 	r.MustRegister(ActiveHttpRequests)
+	r.MustRegister(HttpRequestsSQLInjectionVulnerable)
+	r.MustRegister(HttpRequestsXSSVulnerable)
+	r.MustRegister(HttpRequestsIpWhitelist)
+	r.MustRegister(HttpRequestsIpBlacklist)
+	r.MustRegister(HttpRequestsHeaderFilter)
+	r.MustRegister(HttpRequestsPathFiler)
 }
 
 func NetHttpHandle() {
