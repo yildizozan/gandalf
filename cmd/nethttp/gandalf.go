0322c38b27a992c23fd2dd9f48a69f09d7c83742
Ozan Yıldız <developer@yildizozan.com>
2021-01-27 09:58:35 +0300

    Delete unnecessary old main function

--- cmd/nethttp/gandalf.go
+++ cmd/nethttp/gandalf.go
@@ -11,42 +11,6 @@ import (
 	"net/url"
 )
 
-func main() {
-
-	// Metrics
-	metrics.Init()
-	metrics.NetHttpHandle()
-
-	// Proxy
-	uri, err := url.Parse(viper.GetString("app.host"))
-	if viper.GetBool("verbose") {
-		fmt.Println(uri)
-	}
-	if err != nil {
-		logger.Log("Could not parse downstream url: %s", viper.GetString("app.name"))
-	}
-
-	proxy.Proxy = httputil.NewSingleHostReverseProxy(uri)
-
-	director := proxy.Proxy.Director
-	proxy.Proxy.Director = func(req *http.Request) {
-		director(req)
-		req.Header.Set("X-Forwarded-Host", req.Header.Get("Host"))
-		req.Host = req.URL.Host
-	}
-
-	// Gandalf
-	var cw metrics.ConnectionWatcher
-	http.HandleFunc("/", proxy.Handler)
-	server := &http.Server{
-		Addr:      ":8080",
-		ConnState: cw.OnStateChangeForNetHttp,
-	}
-	if err := server.ListenAndServe(); err != nil {
-		panic(err)
-	}
-}
-
 func Start() {
 	// Metrics
 	metrics.Init()
